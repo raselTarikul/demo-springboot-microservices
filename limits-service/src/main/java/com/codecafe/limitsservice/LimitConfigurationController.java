841de6fb872e513deb3a9aa48e1133945ed0250a
Tarikul Islam <ti.rasel@gmail.com>
2021-03-20 14:36:11 +0600

    Spring cloud config server added

---
+++ limits-service/src/main/java/com/codecafe/limitsservice/LimitConfigurationController.java
@@ -0,0 +1,20 @@
+package com.codecafe.limitsservice;
+
+import com.codecafe.limitsservice.bean.LimitConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitConfigurationController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public LimitConfiguration getLimitConfiguration(){
+        return new LimitConfiguration(configuration.getMinimum(), configuration.getMaximum());
+    }
+
+
+}
